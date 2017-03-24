b8a6e5dc45f846918bb8ea70837910ae8464b389
Jatin Rungta <jatinrungta27@gmail.com>
2017-03-24 15:22:42 +0530

    Implemented Flask server to fetch summary
    The script was not acting as a server. It can be now used to find summary of any page

--- getContentFromURL.py
+++ getContentFromURL.py
@@ -1,20 +1,33 @@
 # Team nameSpace@HINT2017
 
+#
 # This script contains a function which takes a URL as input
 # and returns the content of the content in the webpage
 
 # 'newspaper' library is used here to extract only the main
 # content in a webpage
-
-
+from flask import Flask,render_template
 import urllib
 from newspaper import Article
 
-def getContentFromURL(url):
-	article = Article(url)
+app = Flask(__name__)
+
+@app.route('/<name>')
+def failurl(name):
+	return "Please enter a valid url"
+	
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def getContentFromURL(path):
+	article = Article(path)
 	article.download()
 	article.parse()
-	# article.nlp()
-	# article.summary
-	return article.text
+	article.nlp()
+	article.summary
+	return article.summary 
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
 
