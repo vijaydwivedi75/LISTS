5603f94f96e7e30f28af35eb205ba902d99b57a9
Vijay Prakash Dwivedi <mail@vijaydwivedi.com.np>
2017-03-25 03:25:17 +0530

    Replaced placeholders
    - Added link to the flask app for URL Content Keyword Generation
    - Cleaned Code

--- popup.js
+++ popup.js
@@ -5,6 +5,17 @@
 */
 
 
+// Pre loader jquery code
+jQuery(document).ready(function($) {  
+
+$(window).load(function(){
+  $('#preloader').fadeOut('slow',function(){$(this).remove();});
+});
+
+});
+
+
+
     var source;
     var xmlhttp;
     function getSource(URL){
@@ -23,61 +34,81 @@
     xmlhttp.send();
     return xmlhttp.responseText;
 }
-/*source = getSource('http://crossorigin.me/http://www.vijaydwivedi.com.np')
-document.write(source);*/
 
-var urlCurrent;
+
+var urlCurrent="";
 // Get current URL
-chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true
-}, function(tabs) {
-    // and use that tab to fill in out title and url
-    var tab = tabs[0];
-    document.getElementById('forURL').innerHTML = tab.url;
-   
+function getURL(x, callback){
+  chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true
+  }, function(tabs) {
+      
+      var tab = tabs[0];
+      callback(tab.url);
+     
+  });
+}
+
+
+
+getURL("address", function(x){
+    urlCurrent = x;
+    urlCurrent = "http://flask-javascc.herokuapp.com/" + urlCurrent;
+    console.log(urlCurrent);
+
+    // Extract Keywords from URL
+    var keywords = "";
+
+    function extractKeywords(y, callbacky){
+        
+        var xmlhttp1 = new XMLHttpRequest();
+        xmlhttp1.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                  keywords = xmlhttp1.responseText;          
+
+                  callbacky(keywords);
+            }
+        };
+        console.log(keywords);
+        var URLCurrent = urlCurrent;
+        xmlhttp1.open("GET", URLCurrent, true);
+        xmlhttp1.send();
+    }
+
+    extractKeywords("keys", function(yy){
+        keywords = yy;
+
+
+        // Extract json from URL
+            jsonURL = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBOfvd0PrNoxbShPJvCjPMqbkzzK3ijmP4&q=" + 
+                      keywords + "&cx=010401654321447249722:6jnhkyjk280";
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                      myObj = JSON.parse(this.responseText);
+                      
+                      var div = document.getElementById('card_container');
+                      div.innerHTML = "";
+                    for (var i = 0; i < myObj.items.length; i++) {
+                      data = "<a target='_blank' href=" + myObj.items[i].link+"><div id='card'><img src='"+ myObj.items[i].pagemap.cse_thumbnail[0].src +"'/>"
+                      data1 = "<h2 id='header'>"+ myObj.items[i].title+"</h2>";
+                      data2 = "<p id='snippet'>"+myObj.items[i].htmlSnippet+"</p></div></a>";
+                      
+                      
+                      div.innerHTML = div.innerHTML + data + data1 + data2;
+                      console.log(data2); 
+                    }
+                }
+            };
+
+            jsonURLCROS = jsonURL;
+            xmlhttp.open("GET", jsonURLCROS, true);
+            xmlhttp.send();
+
+    });
+
+    
+
 });
-urlCurrent = "http://blog.ecellmnnit.in/the-hype-cycle-predict-tech-hype-make-smart-decisions";
-console.log(urlCurrent);
-urlCurrent = "http://localhost:5000/" + urlCurrent;
 
-
-// Extract Keywords from URL
-var keywords = ""
-var xmlhttp1 = new XMLHttpRequest();
-xmlhttp1.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-          keywords = xmlhttp1.responseText;          
-          console.log(keywords);
-    }
-};
-console.log(keywords);
-var URLCurrent = /*"http://crossorigin.me/" +*/ urlCurrent;
-xmlhttp1.open("GET", URLCurrent, true);
-xmlhttp1.send();
-
-
-// Extract json from URL
-jsonURL = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBOfvd0PrNoxbShPJvCjPMqbkzzK3ijmP4&q=" + 
-          keywords + "&cx=010401654321447249722:6jnhkyjk280";
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-          myObj = JSON.parse(this.responseText);
-          
-
-        for (var i = 0; i < myObj.items.length / 2; i++) {
-          data = "<a target='_blank' href=" + myObj.items[i].link+"><div id='card'><img src='"+ myObj.items[i].pagemap.cse_thumbnail[0].src +"'/>"
-          data1 = "<h2 id='header'>"+ myObj.items[i].title+"</h2>";
-          data2 = "<p id='snippet'>"+myObj.items[i].htmlSnippet+"</p></div></a>";
-          var div = document.getElementById('card_container');
-          
-          div.innerHTML = div.innerHTML + data + data1 + data2;
-          console.log(data2); 
-        }
-    }
-};
-
-jsonURLCROS = "http://crossorigin.me/" + jsonURL;
-xmlhttp.open("GET", jsonURLCROS, true);
-xmlhttp.send();
